176ba01e475a9be7e11e19a564fa56887ec37c63
Victor Polanco <vmp2408@gmail.com>
2016-11-26 00:02:29 -0500

    Initial Commit

---
+++ Algorithms/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Algorithms
+//
+//  Created by Victor Ml Polanco on 11/26/16.
+//  Copyright © 2016 Victor Ml Polanco. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
