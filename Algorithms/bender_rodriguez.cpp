b80a1645100e1c02077a8d7c4818aa993819cb05
Victor Polanco <vmp2408@gmail.com>
2016-12-01 21:57:09 -0500

    In the middle of solving bender rodriguez

--- Algorithms/bender_rodriguez.cpp
+++ Algorithms/bender_rodriguez.cpp
@@ -6,6 +6,8 @@
 //  Copyright © 2016 Victor Ml Polanco. All rights reserved.
 //
 
+#include "main.h"
+
 #include <map>
 #include <set>
 #include <list>
@@ -38,7 +40,59 @@ int main(int argc, const char * argv[]) {
     freopen("output.out", "w", stdout);
 #endif
     
+    int L;
     
+    map<string, map<string, string>> bends;
     
-    return 0;
+    bends["+x"]["+y"] = "+y";
+    bends["+x"]["-y"] = "-y";
+    bends["+x"]["+z"] = "+z";
+    bends["+x"]["-z"] = "-z";
+    bends["-x"]["+y"] = "-y";
+    bends["-x"]["-y"] = "+y";
+    bends["-x"]["+z"] = "-z";
+    bends["-x"]["-z"] = "+z";
+    bends["+y"]["+y"] = "-x";
+    bends["+y"]["-y"] = "+x";
+    bends["+y"]["+z"] = "+y";
+    bends["+y"]["-z"] = "+y";
+    bends["-y"]["+y"] = "+x";
+    bends["-y"]["-y"] = "-x";
+    bends["-y"]["+z"] = "-y";
+    bends["-y"]["-z"] = "-y";
+    bends["+z"]["+y"] = "+z";
+    bends["+z"]["-y"] = "+z";
+    bends["+z"]["+z"] = "-x";
+    bends["+z"]["-z"] = "+x";
+    bends["-z"]["+y"] = "-z";
+    bends["-z"]["-y"] = "-z";
+    bends["-z"]["+z"] = "+x";
+    bends["-z"]["-z"] = "-x";
+    
+    while (true) {
+        
+        cin >> L;
+        
+        if (L == 0) {
+            break;
+        }
+        
+        L--;
+        
+        string ans, prev = "+x", bend;
+        
+        while (L--) {
+            
+            cin >> bend;
+            
+            if (bend != "No") {
+                
+                ans = bends[prev][bend];
+            
+                prev = ans;
+            }
+        }
+        
+        cout << ans << endl;
+    }
 }
